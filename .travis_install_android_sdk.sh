85dd941ec7a826324358ec0768c0e4e55419538a
Andrew <mrcluster@yandex.ru>
2017-04-16 02:43:41 +0700

    Travis integration

--- .travis_install_android_sdk.sh
+++ .travis_install_android_sdk.sh
@@ -2,13 +2,25 @@
 
 # Script installs Android SDK components
 
-spawn android update sdk --filter tools,platform-tools,build-tools-19.0.0,extra-android-support,extra-google-play_billing,extra-google-m2repository,extra-google-analytics_sdk_v2,extra-google-gcm,extra-google-google_play_services,extra-google-play_apk_expansion,extra-android-m2repository --no-ui --force --all
+spawn android update sdk --filter tools,platform-tools,build-tools-19.0.0,extra-android-support,android-17,sysimg-17,addon-google_apis-google-17,android-19,sysimg-19,addon-google_apis-google-19,addon-google_apis-google-19,extra-google-play_billing,extra-google-m2repository,extra-google-analytics_sdk_v2,extra-google-gcm,extra-google-google_play_services,extra-google-play_apk_expansion,extra-android-m2repository --no-ui --force --all
 expect "Do you accept the license *:"
 send -- "y\r"
-spawn android update sdk --filter android-17,sysimg-17,addon-google_apis-google-17 --no-ui --force --all
 expect "Do you accept the license *:"
 send -- "y\r"
-spawn android update sdk --filter android-19,sysimg-19,addon-google_apis-google-19,addon-google_apis-google-19 --no-ui --force --all
+expect "Do you accept the license *:"
+send -- "y\r"
+expect "Do you accept the license *:"
+send -- "y\r"
+expect "Do you accept the license *:"
+send -- "y\r"
+expect "Do you accept the license *:"
+send -- "y\r"
+expect "Do you accept the license *:"
+send -- "y\r"
+expect "Do you accept the license *:"
+send -- "y\r"
+expect "Do you accept the license *:"
+send -- "y\r"
 expect "Do you accept the license *:"
 send -- "y\r"
 interact
